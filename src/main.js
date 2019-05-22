01c695b21c752d8a77c3b0c35eea381aab7d3491
zhoujin <1602362909@qq.com>
2019-05-22 17:52:46 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router/router'
 import store from './store'
 import './registerServiceWorker'
+import './plugins/mint'
 import './plugins/element.js'
+import './style/index.scss'
+
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+    lintOnSave: false,
+    css: {
+        loaderOptions: {
+            sass: {
+                data: `
+          @import "@/style/color.scss";
+        `
+            }
+        }
+    }
 }
