cf44ecb065ba444a89994cfc4ca7891d9eec85f2
zhoujin <1602362909@qq.com>
2019-05-23 15:51:10 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,34 @@
 module.exports = {
+    // eslint-loader 警告
     lintOnSave: false,
+
+    // css 相关选项
     css: {
+        /*为预处理器 loader 传递自定义选项*/
         loaderOptions: {
             sass: {
-                data: `
-          @import "@/style/color.scss";
-        `
+                data: `@import "@/style/color.scss";`
             }
         }
-    }
+    },
+
+    // 部署应用包基本url
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/production-sub-path/'
+        : './',
+
+    // 配置 webpack-dev-serve 行为
+    devServer: {
+        host: '0.0.0.0',
+        port: 8000,
+        open: 'Chrome',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                pathRewrite: {'^/api': ''}
+            }
+        }
+    },
+
+
 }
